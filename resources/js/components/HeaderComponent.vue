9d28e64c8ab8058c495dc2c70fe57d6014afc6b8
Rahul B <rahul.b@avdevs.com>
2024-08-29 12:25:09 -0600

    header component added

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">Task Management</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
