576cc309baa8539c1607ce8863032ae4028c02cf
Rahul B <rahul.b@avdevs.com>
2024-08-29 11:57:06 -0600

    First Commit with Vue and Bootsrap

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+// import "./bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import App from "../../resources/js/views/App.vue";
+
+import { createApp } from "vue";
+import store from "./store";
+import router from "./router";
+
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(Toast);
+app.mount("#app");
